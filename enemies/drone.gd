6b5db0876b77995e2dacfc4586fe17a9a09b1e27
Silvan <Silvan_Mueller@sluz.ch>
2023-12-02 10:46:56 +0100

    reset to default project

--- enemies/drone.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	# input
-	var direction = Vector2.RIGHT
-
-	velocity = direction * 500
-	move_and_slide()
